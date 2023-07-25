0b196eadbcced534f9e6a16fc431b32f2e8ac978
facusteckler86 <facundomsteckler@gmail.com>
2023-07-25 12:12:43 -0300

    cuarto commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,66 @@
 import { Component } from '@angular/core';
+import { delay, forkJoin, map, of, tap, Observable } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+})
+
+// Uso de observables
+class users {
+
+  obtenerUsuario () {
+
+    return Observable.of([{name: 'Facundo'}])
+    // uso del map
+    .map((users) => users[0])
+    // uso del catch
+    .catch((error) => Observable.throw(error));
+  }
+}
+
+///////////////////////////////
+
+let usersObservable = users.obtenerUsers();
+
+usersObservable.suscribe(
+  (result) => {
+    console.log(result);
+  },
+  (error) =>{
+    console.log(error);
+  }
+);
+
+export class NamePipe implements PipeTransform {
+  transform(value: any, ...args: any[]): any {
+    
+  }
+}
+
+// Uso del ForkJoin
+
+const obs1$ = of(['Facundo','Martin','Josefina']).pipe(delay(4000));
+const obs2$ = of(['001','002','003']).pipe(delay(6000))
+
+// uso de APi
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+forkJoin([
+obs1$,
+obs2$
+
+]).subscribe({
+  next: (nombres, orden) => {
+      this.nombres = nombres;
+      this.orden = orden
+  },
+  complete: () => (this.loading = false)
 })
 export class AppComponent {
   title = 'AngularReactive';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Observable } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,4 +15,20 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  asyncPromise: Promise<string>;
+  asyncObservable: Observable<string>;
+
+  ngOnInit(){
+    this.promesa = this.conPromise('Promise');
+    this.observable = this.conObservable('Observable');
+  }
+  conPromise(value: string): Promise<string>{
+    return new Promise((resolve, reject)=> {
+      setTimeout(() => resolve(value),4000)
+    })
+  }
+  conOberservable(value: string): Observable<string>{
+    return of(value).pipe(delay(3000));
+  }
+} 
